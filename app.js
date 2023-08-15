b0815a4f9886fe1e90e5d1558f1d6336dcc637c4
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-08-15 20:06:18 +0530

    Writing and reading from a file.

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 const http = require("http");
+const fs = require("fs");
+const { parse } = require("path");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text-html");
-  if (req.url === "/home") {
-    res.write("<html>");
-    res.write("<head><title>Welcome</title></head>");
-    res.write("<body><h1>Welcome home!!</h1></body>");
-    res.write("</html>");
-    return res.end();
-  } else if (req.url === "/about") {
-    res.write("<html>");
-    res.write("<head><title>Welcome</title></head>");
-    res.write("<body><h1>Welcome to about us page!!</h1></body>");
-    res.write("</html>");
-    return res.end();
-  } else if (req.url === "/node") {
-    res.write("<html>");
-    res.write("<head><title>Welcome</title></head>");
-    res.write("<body><h1>Welcome to my Node JS project!!</h1></body>");
-    res.write("</html>");
-    return res.end();
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    fs.readFile("message.txt", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.write("<html>");
+      res.write("<head><title>Welcome</title></head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      // console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      // console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
 });
 
