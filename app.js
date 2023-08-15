bcf42518a415ca93a5c34d4170bb9121f06e3404
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-08-15 11:20:32 +0530

    Request / Response.

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+const http = require("http");
 
-const http = require('http')
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text-html");
+  if (req.url === "/home") {
+    res.write("<html>");
+    res.write("<head><title>Welcome</title></head>");
+    res.write("<body><h1>Welcome home!!</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (req.url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>Welcome</title></head>");
+    res.write("<body><h1>Welcome to about us page!!</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (req.url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>Welcome</title></head>");
+    res.write("<body><h1>Welcome to my Node JS project!!</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+});
 
-const server = http.createServer( (req, res)=> {
-    res.write('Ramsankar')
-    res.end()
-    console.log('RAMSANKAR')
-})
-
-server.listen(4000)
+server.listen(4000);
